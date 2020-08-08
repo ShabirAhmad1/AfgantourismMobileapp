104b4e6351ae769ccca21ff18e1add83641192b4
Abdullah <abdullahjaffer8@bitbucket.org>
2020-08-08 23:31:14 +0500

    initial commit

---
+++ src/app/Maincomponent/app.component.ts
@@ -0,0 +1,15 @@
+import { Component,OnInit } from '@angular/core';
+declare var device;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'FrontendAfghanTourism';
+  ngOnInit() {
+    document.addEventListener('deviceready', function() {
+    alert(device.platform);
+    }, false);
+    }
+}
